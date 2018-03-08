f60bcbfc8efd6fab2c3200ad8a335df3827f13c0
pos0637 <pos0637@163.com>
2018-03-08 23:39:29 +0800

    兼容ie8.

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var copyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: {
-        main: './src/scripts/main.js'
+        main: ['babel-polyfill', 'es6-promise', 'fetch-ie8', './src/scripts/main.js']
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -15,24 +15,14 @@ module.exports = {
         loaders: [
             {
                 test: /\.js$/,
-                loader: 'babel-loader',                
+                loader: 'babel-loader',
                 include: path.resolve(__dirname, 'src'),
-                exclude: path.resolve(__dirname, 'node_modules'),
-                query: {
-                    presets: ['latest']
-                }
+                exclude: path.resolve(__dirname, 'node_modules')
             },
             {
-                test: /bootstrap-sass[\/\\]assets[\/\\]javascripts[\/\\]/,
-                loader: 'imports-loader?jQuery=jquery'
-            },
-            {
-                test: /\.(woff2?|svg)$/,
-                loader: 'url-loader?limit=10000'
-            },
-            {
-                test: /\.(ttf|eot)$/,
-                loader: 'file-loader'
+                test: /\.js$/,
+                loaders: ['es3ify-loader'],
+                enforce: 'post'
             }
         ]
     },
@@ -47,7 +37,8 @@ module.exports = {
             from: path.resolve(__dirname, 'html/vendors'),
             to: path.resolve(__dirname, 'dist/vendors'),
             type: 'dir'
+
         }])
     ],
     devtool: 'cheap-module-eval-source-map'
-}
+};
